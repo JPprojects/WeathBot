91c19d0b94c473b9f043a742cfef6d15621775d4
JPprojects <jerome791@hotmail.co.uk>
2022-08-18 17:01:15 +0100

    update api with correct call and create private function for the call

--- WeatherBot/Controllers/HomeController.cs
+++ WeatherBot/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Diagnostics;
 using WeatherBot.Models;
 
@@ -15,6 +16,7 @@ namespace WeatherBot.Controllers
 
         public IActionResult Index()
         {
+            apiCall();
             return View();
         }
 
@@ -27,6 +29,22 @@ namespace WeatherBot.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private void apiCall()
+        {
+            HttpClient client = new HttpClient();
+            var url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=a3bec6cae5be523e8f91b317166bfe18";
+
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+
+            if (response.IsSuccessStatusCode)
+            {
+                var readableResult = response.Content.ReadAsStringAsync();
+                // Parse the response body.
+                Console.WriteLine(readableResult);
+            }
         }
     }
 }
