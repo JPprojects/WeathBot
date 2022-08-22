de906a6bf6d64ce3b27688296780b997afc60178
JPprojects <jerome791@hotmail.co.uk>
2022-08-22 20:47:44 +0100

    add comment to describe why

--- WeatherBot/Models/HomeViewModel.cs
+++ WeatherBot/Models/HomeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿namespace WeatherBot.Models
 {
+    //Inherit weather model to remove logic from model builder to map across, Status being set to know what the staus of the call is to the user
     public class HomeViewModel: WeatherModel
     {
         public string Status { get; set; }
